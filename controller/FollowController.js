850378ad195a8d6d2d82a369d1d347f12ca3e951
Adem Ben Amor <dissojak@gmail.com>
2024-03-28 17:30:58 +0100

    adem: follow and unfollow functions

--- controller/FollowController.js
+++ controller/FollowController.js
@@ -24,3 +24,55 @@ exports.isFollowing = asyncHandler(async (req, res, next) => {
     throw new Error("Failed to check follow status");
   }
 });
+
+/**
+ * @desc    Follow an artist
+ * @route   POST /api/follow/followArtist/:artistId
+ * @access  Private
+ */
+exports.followArtist = async (req, res, next) => {
+  const clientId = req.user._id;
+  const artistId = req.params.artistId;
+
+  try {
+    // Check if the client is already following the artist
+    const isFollowing = await Follow.findOne({ clientId, artistId });
+    if (isFollowing) {
+      return next(new HttpError('You are already following this artist', 400));
+    }
+    const follow = new Follow({
+      clientId,
+      artistId,
+    });
+    await follow.save();
+
+    res.status(201).json({ message: `You are now following artist ${artistId}` });
+  } catch (err) {
+    return next(new HttpError('Failed to follow the artist', 500));
+  }
+};
+
+/**
+ * @desc    Unfollow an artist
+ * @route   DELETE /api/follow/unfollowArtist/:artistId
+ * @access  Private
+ */
+exports.unfollowArtist = async (req, res, next) => {
+  const clientId = req.user._id;
+  const artistId = req.params.artistId; // Extract artistId from URL parameter
+
+  try {
+    // Check if the client is currently following the artist
+    const isFollowing = await Follow.findOne({ clientId, artistId });
+    if (!isFollowing) {
+      return next(new HttpError('You are not currently following this artist', 400));
+    }
+
+    // Delete the follow document
+    await Follow.findOneAndDelete({ clientId, artistId });
+
+    res.status(200).json({ message: `You have unfollowed artist ${artistId}` });
+  } catch (err) {
+    return next(new HttpError('Failed to unfollow the artist', 500));
+  }
+};
--- routes/follow.js
+++ routes/follow.js
@@ -5,5 +5,8 @@ const { check } = require("express-validator");
 const router = express.Router();
 
 router.get("/isFollowing",MW.protect,FC.isFollowing);
+router.post("/followArtist/:artistId",MW.protect,FC.followArtist);
+router.delete("/unfollowArtist/:artistId",MW.protect,FC.unfollowArtist);
+
 
 module.exports = router;
