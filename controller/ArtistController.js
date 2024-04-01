7fa3a495ab6eac61c7c2fa8d13a5ed1e2d80a90b
Adem Ben Amor <dissojak@gmail.com>
2024-04-01 16:37:04 +0100

    update order statu for artist

--- controller/ArtistController.js
+++ controller/ArtistController.js
@@ -1,5 +1,34 @@
 const HttpError = require("../models/http-error");
 const { validationResult } = require("express-validator");
+const asyncHandler = require("express-async-handler");
 const Artist = require("../models/user");
 
-// Add your controller methods here
+exports.openOrder = asyncHandler(async (req, res, next) => {
+    const { normalPrice, rapidPrice } = req.body;
+    const artistId = req.user._id;
+  
+    try {
+      const artist = await Artist.findOneAndUpdate(
+        { _id: artistId },
+        {
+          orderStatus: true,
+          normalPrice: normalPrice,
+          rapidPrice: rapidPrice,
+        },
+        { new: true }
+      );
+  
+      if (!artist) {
+        return next(new HttpError("Artist not found", 404));
+      }
+  
+      res.status(200).json({
+        message: "Order statu updated successfully",
+        artist,
+      });
+    } catch (error) {
+      return next(
+        new HttpError("Failed to update order statue for this artist", 500)
+      );
+    }
+  });
