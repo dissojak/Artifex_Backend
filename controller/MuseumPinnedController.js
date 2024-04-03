7d1177e708fff7d2edda5b9e3e0fde85855d7bb0
Adem Ben Amor <dissojak@gmail.com>
2024-04-03 15:27:29 +0100

    add controller to manage pinned museums

---
+++ controller/MuseumPinnedController.js
@@ -0,0 +1,8 @@
+const HttpError = require("../models/http-error");
+const { validationResult } = require("express-validator");
+const MuseumPinned = require("../models/museumPinned");
+const Museum = require("../models/museum");
+const User = require("../models/user");
+
+// Add your controller methods here
+
